57bf56a18d61adafdbec8cf1419751bc5a6d420a
flo <flo@MacBook-Pro-de-ARIEL.local>
2016-02-27 17:10:10 -0600

    showcase module

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# modelo
-ModeloNow
+```javascript
+	const LayoutView = Marionette.LayoutView.extend({
+	template: false,
+	el: 'body',
+	regions: {
+	  preview: '#module'
+	},
+	initialize() {
+	  this.render();
+	},
+	});
+
+	const layout = new LayoutView();
+	articleList = [
+	{
+	  name: 'Corona',
+	  description: '4.9%',
+	  price: 2.05,
+	  stars: 4.9,
+	  quantity: 0,
+	  image: 'http://vignette4.wikia.nocookie.net/beer/images/6/6b/Corona.jpg/revision/latest?cb=20130322234121'
+	},
+	{
+	  name: 'Victoria',
+	  description: '4.9%',
+	  price: 2.05,
+	  stars: 4.5,
+	  quantity: 0,
+	  image: 'http://www.tragos-tragos.com/wp-content/uploads/2012/09/cerveza-victoria.jpg'
+	}
+	];
+	const collection = new Backbone.Collection(articleList);
+	const articleListView = new Marionette.ArticleList({
+	collection: collection
+	});
+	layout.getRegion('preview').show(articleListView);
+``
---
+++ modules/showcase/src/article/index.js
@@ -0,0 +1,42 @@
+var Article = Marionette.ItemView.extend({
+  template: require('./templates/article.hbs'),
+  className: 'mdl-cell mdl-cell--4-col mdl-cell--6-col-tablet mdl-cell--12-col-phone',
+  events: {
+  	'click .add-article': 'addArticle',
+  	'click .remove-article': 'removeArticle'
+  },
+  modelEvents: {
+  	'change:quantity': 'renderCurrentPrice'
+  },
+  initialize: function() {
+  	var quantity = 0;
+  	this.model.set('quantity', quantity);
+  	this.setPrice()
+  },
+  setPrice: function() {
+  	var quantity = this.model.get('quantity');
+  	var price = this.model.get('price');
+  	var currentPrice = (price * quantity).toFixed(2);
+  	this.model.set('currentPrice', currentPrice);
+  },
+  renderCurrentPrice: function() {
+  	this.setPrice();
+  	this.$el.find('.current-price').html('$ '+this.model.get('currentPrice'));
+  	this.$el.find('.article-quantity').val(this.model.get('quantity'));
+  },
+  addArticle: function() {
+  	console.log('add article')
+  	var currentQuantity = this.model.get('quantity');
+  	currentQuantity++;
+  	this.model.set('quantity', currentQuantity);
+  },
+  removeArticle: function() {
+  	var currentQuantity = this.model.get('quantity');
+  	if (currentQuantity > 0) {
+  		currentQuantity--;
+  		this.model.set('quantity', currentQuantity);
+  	}
+  }
+});
+
+module.exports = Article
