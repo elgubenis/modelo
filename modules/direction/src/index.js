8c2541aa7cb1a18f6490395bd7ab9daf3fb233dd
Linus Gubenis <elgubenis@gmail.com>
2016-02-27 13:52:17 -0600

    Estas aqui?

--- modules/direction/src/index.js
+++ modules/direction/src/index.js
@@ -2,15 +2,40 @@
 'use strict';
 
 function initMap() {
-  new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 12
   });
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      var infoWindow = new google.maps.InfoWindow({map: map});
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Estas aqui?');3
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
 
 Marionette.DirectionView = Marionette.ItemView.extend({
   template: require('./template.hbs'),
-  onRender() {
+  onShow() {
     initMap();
   },
 });
