5a60b3a03db2207543fda1aecdd9e5b90545708a
Linus Gubenis <elgubenis@gmail.com>
2016-02-28 06:52:21 -0600

    ml

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -48,7 +48,7 @@ module.exports = function(router){
       .then(function(result){
         Users.findByIdAndUpdate(order.userId, {
           $inc: {
-            mm: 50
+            ml: 50
           },
           $push: {
             'orders': result
