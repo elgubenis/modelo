4a25eff89500798978d40cfa6885037b357e64dc
Jesus <jesus@localhost.localdomain>
2016-02-27 23:58:53 -0600

    select name

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -14,7 +14,7 @@ module.exports = function(router){
 
   router.route('/users/:_id').get(function(req, res){
     Users.findById(req.params._id)
-    .select('fullName image')
+    .select('name fullName image')
     .then(function(result){
       res.send(result);
     })
