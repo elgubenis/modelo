0dc76bed52efc5cef1f34e0467c2416e3bbed6d7
Beto Sosa <betososah@gmail.com>
2016-02-28 01:33:00 -0600

    conf view

---
+++ modules/confirmation/src/index.js
@@ -0,0 +1,9 @@
+/* globals Marionette */
+
+import template from './template.hbs';
+
+Marionette.Modelo = Marionette.Modelo || {};
+
+Marionette.Modelo.ConfirmationView = Marionette.ItemView.extend({
+  template,
+});
