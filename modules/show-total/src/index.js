ff8d7f7dc2a5cee081987e92ff6154c340d58575
flo <flo@MacBook-Pro-de-ARIEL.local>
2016-02-27 18:13:53 -0600

    show total

--- modules/show-total/src/index.js
+++ modules/show-total/src/index.js
@@ -1,5 +1,24 @@
+Marionette.ShowTotal = Marionette.ItemView.extend({
+	template: require('./templates/show-total.hbs'),
+	className: 'show-total',
+	initialize: function(options) {
+		this.total = options.total;
+	},
+	serializeData: function() {
+		return {
+			total: this.total
+		};
+	},
+	onShow: function() {
+		var self = this;
+		this.$el.addClass('tada animated');
+		setTimeout(function(){
+			self.$el
+			.addClass('fadeOut');
+		}, 1000);
+		setTimeout(function(){
+			self.destroy();
+		}, 1500);
+	}
 
-
-Marionette.Toast = Marionette.CollectionView.extend({
-  
 });
