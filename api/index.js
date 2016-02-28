2c56cf3f0d24cd89ba15dab32008df96f1739e8e
Linus Gubenis <elgubenis@gmail.com>
2016-02-28 00:48:45 -0600

    preflight handling

--- api/index.js
+++ api/index.js
@@ -8,7 +8,21 @@ var express    = require('express'),
 // connect database
 mongoose.connect(config.db.database);
 // config api
+
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    // Set custom headers for CORS
+    res.header("Access-Control-Allow-Headers", "Content-type,Accept");
+    return res.status(200).end();
+  } else {
+    next();
+  }
+});
+
 app.use(corser.create());
+
 app.set('port', 1337)
 
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
