403280df193656a7177f346c950ec4e74f2f8fb3
Beto Sosa <betososah@gmail.com>
2016-02-27 23:33:48 -0600

    buttons view

--- modules/button/src/index.js
+++ modules/button/src/index.js
@@ -1,9 +1,39 @@
-/* globals Marionette */
+/* globals Marionette, Backbone, _ */
+
 
 Marionette.Modelo = Marionette.Modelo || {};
 
-Marionette.Modelo.Button = Marionette.ItemView.extend({
-  template: require('./template.hbs'),
-  tagName: 'button',
-  className: 'mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--colored',
+import template from './template.hbs';
+// import ButtonCollectionView from './collection.js';
+// import PrimaryButton from './primary.js';
+
+Marionette.Modelo.Button = Marionette.LayoutView.extend({
+  template,
+  className: 'mdl-speed-dial mdl-speed-dial--bottom-fixed',
+
+  initialize(options) {
+    this.options = options;
+    this.model = new Backbone.Model();
+    this.model.set(options);
+  },
+
+  events: {
+    'click #addOrder': 'addOrder',
+    'click #addSchedule': 'addSchedule',
+  },
+
+  addOrder() {
+    const addOrder = this.model.get('addOrder');
+    if (_.isFunction(addOrder)) {
+      addOrder.call(this);
+    }
+  },
+
+  addSchedule() {
+    const addSchedule = this.model.get('addSchedule');
+    if (_.isFunction(addSchedule)) {
+      addSchedule.call(this);
+    }
+  },
+
 });
--- modules/button/src/mod/index.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-  console.log('im the default');
-};
