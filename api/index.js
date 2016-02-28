72e298f1d5dfb2808f507702836fbae4712ab69b
Jesus <jesus@localhost.localdomain>
2016-02-27 18:07:05 -0600

    add corser

--- api/index.js
+++ api/index.js
@@ -3,20 +3,21 @@ var express    = require('express'),
     app        = express(),
     bodyParser = require('body-parser'),
     config     = require('./config'),
+    corser = require('corser'),
     mongoose   = require('mongoose');
 // connect database
 mongoose.connect(config.db.database);
 // config api
+var router = express.Router();
+app.use(router)
 app.set('port', 1337)
+app.use(corser.create());
 app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
 app.use(bodyParser.json({limit: '5mb'}));
 app.use(function(req, res, next) {
   res.header('X-Modelo-now');
   next();
 });
-// == Router ==
-var router = express.Router();
-app.use(router)
 // == endpoints ==
 var articles = require('./controllers/articles')(router);
 var orders   = require('./controllers/orders')(router);
